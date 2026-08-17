9b75e37e3453a31d7029e14f0502a4f8fbef5e82
Olya <fialkaolya08@gmail.com>
2026-08-17 14:58:23 +0200

    Changed solution without css styles only calculate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,14 @@ const wall = document.querySelector('.wall');
 
 const spider = document.querySelector('.spider');
 
+const topWall = wall.clientHeight / 2 - spider.offsetHeight / 2;
+
+const leftWall = wall.clientWidth / 2 - spider.offsetWidth / 2;
+
 wall.style.position = 'relative';
 
-spider.style.cssText = `
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%)`;
+spider.style.position = 'absolute';
+
+spider.style.top = `${topWall}px`;
+
+spider.style.left = `${leftWall}px`;
