8a3bd4af5b83793a3c9b85e8c8dd2611e0b2eb16
Olya <fialkaolya08@gmail.com>
2026-08-17 14:39:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+
+spider.style.cssText = `
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%)`;
